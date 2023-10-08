2b267cd772d4c5334ef244f854d90e829e33104c
cowbuno_fedora <talipov.zhenis@gmail.com>
2023-10-08 21:39:45 +0600

    factory

--- factory/main/main.go
+++ factory/main/main.go
@@ -7,9 +7,25 @@ import (
 
 func main() {
 
-	mysql := factory.CreateDatabase("MySQL")
+	fct := factory.DatabaseConnectionFactory{}
+
+	mysql := fct.CreateDatabase("MySQL")
 	mysql.Connect()
 
 	mysqlRes, _ := mysql.Query("select")
 	fmt.Println(mysqlRes)
+	fmt.Println()
+
+	postgreSQL := fct.CreateDatabase("PostgreSql")
+	postgreSQL.Connect()
+
+	postgreSQLlRes, _ := postgreSQL.Query("select")
+	fmt.Println(postgreSQLlRes)
+	fmt.Println()
+
+	mongoDB := fct.CreateDatabase("MongoDB")
+	mongoDB.Connect()
+
+	mongoDBRes, _ := mongoDB.Query("select")
+	fmt.Println(mongoDBRes)
 }
